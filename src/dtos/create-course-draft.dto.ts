c277fd0faf08e4d89286390fe7305540a00f4f21
VN Phạm <nam.pv1805@gmail.com>
2024-08-13 14:40:43 +0700

    update

--- src/dtos/create-course-draft.dto.ts
+++ src/dtos/create-course-draft.dto.ts
@@ -9,65 +9,49 @@ import {
 import { FileInformation } from 'src/entities/file-information.entity';
 
 class CourseMember {
-    @IsString()
     @IsOptional()
     name?: string;
 
-    @IsString()
     @IsOptional()
     role?: string;
 
-    @IsString()
     @IsOptional()
     link?: string;
 
-    @IsString()
     @IsOptional()
     publicKey?: string;
 }
 
 export class CreateCourseDraftDto {
-    @IsString()
     @IsOptional()
     name?: string;
 
-    @IsUrl()
     @IsOptional()
     avatarImage?: string;
 
-    @IsUrl()
     @IsOptional()
     coverImage?: string;
 
-    @IsString()
     @IsOptional()
     publicKey?: string;
 
-    @IsString()
     @IsOptional()
     description?: string;
 
-    @IsString()
     @IsOptional()
     problemStatement?: string;
 
-    @IsString()
     @IsOptional()
     solution?: string;
 
-    @IsString()
     @IsOptional()
     challengeAndRisk?: string;
 
-    @IsArray()
-    @ValidateNested({ each: true })
     @Type(() => CourseMember)
     @IsOptional()
     members?: CourseMember[];
 
-    @IsArray()
-    @IsOptional()
-    @ValidateNested({ each: true })
     @Type(() => FileInformation)
+    @IsOptional()
     documents?: FileInformation[];
 }
